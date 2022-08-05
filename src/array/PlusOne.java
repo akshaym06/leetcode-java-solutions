5f9964bfd2396ae037de3cc5da98d3eb8d28232a
M <Akshay.M@t-systems.com>
2022-08-05 17:41:13 +0530

    Update PlusOne.java

--- src/array/PlusOne.java
+++ src/array/PlusOne.java
@@ -1,12 +1,18 @@
 package array;
 
+import java.util.Arrays;
+
 public class PlusOne {
 
     public static void main(String[] args) {
+        int[] array1 = new int[]{1,2,3};
+        int[] array2 = new int[]{1,9};
 
+        System.out.println(Arrays.toString(plusOne(array1)));
+        System.out.println(Arrays.toString(plusOne(array2)));
     }
 
-    public int[] plusOne(int[] digits) {
+    public static int[] plusOne(int[] digits) {
         int len = digits.length;
 
         for (int i = len-1; i>=0; i--) {
