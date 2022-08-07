0f8896c9c9ee545a5b3f5f290fa4f0853c5c506b
M <Akshay.M@t-systems.com>
2022-08-07 17:38:56 +0530

    committing 141. Linked List Cycle
    committing 141. Linked List Cycle

--- README.md
+++ README.md
@@ -22,3 +22,4 @@
 
 - [21. Merge Two Sorted Lists](https://github.com/akshaym06/leetcode-java-solutions/blob/main/src/linkedlist/MergeTwoSortedLists.java) (Easy)
 - [83. Remove Duplicates from Sorted List](https://github.com/akshaym06/leetcode-java-solutions/blob/main/src/linkedlist/RemoveDuplicates.java) (Easy)
+- [141. Linked List Cycle](https://github.com/akshaym06/leetcode-java-solutions/blob/main/src/linkedlist/LinkedListCycle.java) (Easy)
---
+++ src/linkedlist/LinkedListCycle.java
@@ -0,0 +1,36 @@
+package linkedlist;
+
+public class LinkedListCycle {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next;
+
+        System.out.println(hasCycle(head));
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+
+}
