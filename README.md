e6caa9f2242b27c106ad0e6d52118840cd868f0c
M <Akshay.M@t-systems.com>
2022-08-21 15:03:32 +0530

    committing 206. Reverse Linked List
    committing 206. Reverse Linked List

--- README.md
+++ README.md
@@ -25,3 +25,4 @@
 - [141. Linked List Cycle](https://github.com/akshaym06/leetcode-java-solutions/blob/main/src/linkedlist/LinkedListCycle.java) (Easy)
 - [160. Intersection of Two Linked Lists](https://github.com/akshaym06/leetcode-java-solutions/blob/main/src/linkedlist/Intersection.java) (Easy)
 - [203. Remove Linked List Elements](https://github.com/akshaym06/leetcode-java-solutions/blob/main/src/linkedlist/RemoveElements.java) (Easy)
+- [206. Reverse Linked List](https://github.com/akshaym06/leetcode-java-solutions/blob/main/src/linkedlist/ReverseLinkedList.java) (Easy)
---
+++ src/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,50 @@
+package linkedlist;
+
+public class ReverseLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        head = prev;
+        return head;
+    }
+
+    public static void printList(ListNode node) {
+        ListNode temp = node;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        printList(head);
+
+        ListNode node = reverseList(head);
+
+        printList(node);
+    }
+}
