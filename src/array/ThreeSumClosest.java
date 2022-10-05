8ed9110a603ff47ea52ec16e208e722940a919bc
M <akshaymnavada06@gmail.com>
2022-10-05 15:18:20 +0530

    committing 16. 3Sum Closest
    committing 16. 3Sum Closest

---
+++ src/array/ThreeSumClosest.java
@@ -0,0 +1,35 @@
+package array;
+
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+    public static int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int diff = Integer.MAX_VALUE;
+        int res = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (diff > Math.abs(target - sum)) {
+                    diff = Math.abs(target - sum);
+                    res = sum;
+                } else if (sum == target) {
+                    return sum;
+                } else if (sum > target) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1, 2, 1, -4};
+        int target = 1;
+        System.out.println(threeSumClosest(nums, target));
+    }
+}
